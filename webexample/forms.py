ae845d1fe8c413ab9e7bfad70e39a200ba7a7776
никита иванов <evilangel3211@gmail.com>
2022-09-11 22:34:38 +0500

    little fix

--- webexample/forms.py
+++ webexample/forms.py
@@ -3,6 +3,6 @@ from django import forms
 
 class PriceForm(forms.Form):
     order = forms.CharField()
-    price_usa = forms.IntegerField()
-    price_rus = forms.FloatField()
+    price_usa = forms.DecimalField()
+    price_rus = forms.DecimalField()
     delivery_date = forms.DateTimeField()
